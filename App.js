6c2b730af565d2c2ae173b5dbfb4a696e5629ec1
Therese Grass <tg222kv@student.lnu.se>
2022-12-14 16:12:05 +0100

    add code in App.js and PrimaryButton.js

--- App.js
+++ App.js
@@ -1,21 +1,76 @@
+import { StyleSheet , ImageBackground, SafeAreaView } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
+import { useState } from 'react'
+import { useFonts } from 'expo-font'
 
-import { StyleSheet, Text, View } from 'react-native';
-
-
+import AppLoading from 'expo-app-loading'
+import StartGameScreen from './screens/StartGameScreen'
+import GameScreen from './screens/GameScreen'
+import GameOverScreen from './screens/GameOverScreen';
+import Colors from './constants/colors'
 
 export default function App() {
+  const [userNumber, setUserNumber] = useState()
+  const [gameIsOver, setGameIsOver] = useState(true)
+  const [guessRounds, setGuessRounds] = useState(0)
+
+  const [fontsLoaded] = useFonts({
+    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf')
+  })
+
+  if (!fontsLoaded) {
+    return <AppLoading />
+  }
+ 
+  function pickedNumberHandler(pickedNumber) {
+    setUserNumber(pickedNumber)
+    setGameIsOver(false)
+  }
+ 
+  function gameOverHandler(rounds) {
+    setGameIsOver(true)
+    setGuessRounds(rounds)
+   
+  }
+
+  function startNewGameHandler() {
+    setUserNumber(null)
+    setGuessRounds(0)
+  }
+
+  let screen = <StartGameScreen onPickNumber={pickedNumberHandler} />
+
+  if (userNumber) {
+    screen = <GameScreen userNumber={userNumber} onGameOver={gameOverHandler} />
+  }
+
+  if (gameIsOver && userNumber) {
+    screen = <GameOverScreen userNumber={userNumber} roundsNumber={guessRounds} onStartNewGame={startNewGameHandler}></GameOverScreen>
+  }
+
+  // safeAreaView makes sure that there is a distance between the top edge / nodge and the content
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <LinearGradient  colors={[Colors.primary700, Colors.accent500]} style={styles.rootScreen}>
+      <ImageBackground 
+      source={require('./assets/images/dices.png')} 
+      resizeMode="cover" 
+      style={styles.rootScreen}
+      imageStyle={styles.backgroundImage}>
+        <SafeAreaView style={styles.rootScreen}>
+          {screen}
+        </SafeAreaView>
+      </ImageBackground>
+    </LinearGradient>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+ rootScreen: {
+   flex: 1,
+ },
+ backgroundImage: {
+   opacity: 0.2
+ }
+
 });
--- components/PrimaryButton.js
+++ components/PrimaryButton.js
@@ -1,15 +1,48 @@
-import { View, Text } from 'react-native'
+import { View, Text, Pressable, StyleSheet } from "react-native"
+import Colors from '../constants/colors'
 
-function PrimaryButton({children}) {
-  return (
-  <View>
-    <Text>
-      {children}
-    </Text>
-  </View>
-  )
+function PrimaryButton({ children, onPressed }) { // this is destruction, you could also write props and then write props.children in the text element.
+    function pressedButtonHandler() {
+
+
+    }
+   
+    return (
+      <View style={styles.buttonOuterContainer}>
+        <Pressable 
+          style={({ pressed }) => pressed ? [styles.buttonInnerContainer, styles.pressed] : styles.buttonInnerContainer}
+          onPress={onPressed} // onPressed is a prop in the <Pressable> component. OnPress points to a method (confirmInputHandler) found in StartGameScreen file.
+          android_ripple={{ color: Colors.primary600 }}>
+          <Text style={styles.buttonText}>{children}</Text>
+        </Pressable>
+      </View>
+    )
+}
+export default PrimaryButton
+
+const styles = StyleSheet.create({
+
+  buttonOuterContainer: {
+    margin: 4,
+    borderRadius: 28,
+    overflow: 'hidden'
+  },
+
+  buttonInnerContainer: {
+    borderRadius: 28,
+    backgroundColor: Colors.primary500,
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    elevation: 2,
+  },
+  buttonText: {
+    color: 'white',
+    textAlign: 'center'
+  },
+pressed: {
+  opacity: 0.75
 }
 
-export default PrimaryButton
+})
 
 // onPress is a prop and is found on the onPress in the <Pressable> component. OnPress points to a method (confirmInputHandler) found in StartGameScreen file.
